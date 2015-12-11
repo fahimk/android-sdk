50731b5826c748a4d4eed740d85903a3c1cd4880
marek1985 <marek1985@gmail.com>
2015-12-11 13:41:13 +0100

    final merge fix into new branch

---
+++ yesgraph/src/main/java/com/yesgraph/android/models/FullDetailsContact.java
@@ -0,0 +1,160 @@
+package com.yesgraph.android.models;
+
+/**
+ * Created by Dean Bozinoski on 12/4/2015.
+ */
+public class FullDetailsContact {
+    private String id;
+    private String name;
+    private String email;
+    private String email_type;
+    private String phone;
+    private String note;
+    private String poBox;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private String type;
+    private String imName;
+    private String imType;
+    private String orgName;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail_type() {
+        return email_type;
+    }
+
+    public void setEmail_type(String email_type) {
+        this.email_type = email_type;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getPoBox() {
+        return poBox;
+    }
+
+    public void setPoBox(String poBox) {
+        this.poBox = poBox;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImName() {
+        return imName;
+    }
+
+    public void setImName(String imName) {
+        this.imName = imName;
+    }
+
+    public String getImType() {
+        return imType;
+    }
+
+    public void setImType(String imType) {
+        this.imType = imType;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ yesgraph/src/main/java/com/yesgraph/android/models/RecentlyContactedContact.java
@@ -0,0 +1,35 @@
+package com.yesgraph.android.models;
+
+/**
+ * Created by Dean Bozinoski on 12/4/2015.
+ */
+public class RecentlyContactedContact {
+
+    private String name;
+    private String number;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
